4c548c266fe4baace3219045325fb0955f677cda
Денис Иванов <deni_95@list.ru>
2022-02-21 22:28:25 +0500

    yatube

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
-    return HttpResponse('Группы постов')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
