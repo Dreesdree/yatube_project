40d67a0a4906dfca9f34af8683da285f6f9d7841
Денис Иванов <77634046+Dreesdree@users.noreply.github.com>
2022-02-26 11:47:22 +0500

    Update urls.py

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,5 +7,4 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index' ),
     path('group/<slug:slug>/', views.group_posts, name='group_lists'),
-    path('group_list', views.group_list, name='group_list')
 ]
